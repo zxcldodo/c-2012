c1c1280888f78145f70a4832fa575082a901b93b
dodo <148619694+zxcldodo@users.noreply.github.com>
2023-10-21 13:03:54 +0300

    Create aaaaaaa.py

---
+++ aaaaaaa.py
@@ -0,0 +1,7 @@
+a = ["ananas", "apple", "peach", "candy", "pear", "maracuja", "lemon", "sweets"]
+updated_list = []
+for i in a:
+    if i != "candy":
+        updated_list.append(i)
+print(updated_list)
+
