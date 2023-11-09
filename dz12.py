a0593bc2854c7543801363b9de59f3c2d58fbc1d
dodo <148619694+zxcldodo@users.noreply.github.com>
2023-11-09 19:42:12 +0200

    Add files via upload

---
+++ dz12.py
@@ -0,0 +1,20 @@
+def find_min_coins(coins, target_amount):
+    coins.sort()
+    num_coins = 0
+    coin_combination = []
+    for coin in coins:
+        num_coin = target_amount // coin
+        num_coins += num_coin
+        target_amount -= num_coin * coin
+        coin_combination.append((coin, num_coin))
+        if target_amount == 0 or len(coin_combination) == 2:
+            break
+    return num_coins, coin_combination
+coins = list(map(int, input("Print nominal cherez space: ").split()))
+target_amount = int(input("Print sum: "))
+
+num_coins, coin_combination = find_min_coins(coins, target_amount)
+print(f"Min col v monet: {num_coins}")
+print("Kombination i if coll:")
+for coin, num in coin_combination:
+    print(f"nominal {coin}: {num} coina")
